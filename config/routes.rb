268de83d33e8bc7f7a0bd9916159a3045da7acc2
Agustin Morere <amorere10@gmail.com>
2022-11-28 10:57:29 -0300

    avance

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'list/new'
+  get 'list/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-end
+  resources :list, except: [:edit, :update] do
+      resources :bookmark, only: %i[new create]
+    end
+  resources :bookmark, only: %i[destroy]
+  end
